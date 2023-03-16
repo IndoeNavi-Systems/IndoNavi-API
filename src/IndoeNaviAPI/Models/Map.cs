97059fc8a434ff795c8741ca9b71f9c2daf58fd6
Jonas Hofmann Hansen <jonas.hansen@visma.com>
2023-03-16 08:28:55 +0100

    Created models

---
+++ src/IndoeNaviAPI/Models/Map.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace IndoeNaviAPI.Models;
+
+public class Map
+{
+	public string Area { get; set; }
+	public Bitmap Image { get; set; } 
+	public List<RouteNode> RouteNodes { get; set; }
+	public List<SPE> SPEs { get; set; }
+}
---
+++ src/IndoeNaviAPI/Models/RouteNode.cs
@@ -0,0 +1,11 @@
+﻿namespace IndoeNaviAPI.Models;
+
+public class RouteNode
+{
+	public double X { get; set; }
+	public double Y { get; set; }
+	public bool IsDestination { get; set; }
+	public List<RouteNode> RouteNodes { get; set; }
+	public string Name { get; set; }
+	
+}

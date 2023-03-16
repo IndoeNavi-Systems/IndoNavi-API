f0436f60abd9e1915e5797f71e5721e7868c227c
Jonas Hofmann Hansen <jonas.hansen@visma.com>
2023-03-16 09:06:33 +0100

    Created map controller

---
+++ src/IndoeNaviAPI/Utilities/Utility.cs
@@ -0,0 +1,10 @@
+﻿namespace IndoeNaviAPI.Utilities;
+
+public class Utility
+{
+	public static bool IsBase64String(string base64)
+	{
+		Span<byte> buffer = new(new byte[base64.Length]);
+		return Convert.TryFromBase64String(base64, buffer, out _);
+	}
+}

c68e4613db728e9ea9de2f4a434b8cc4bb476b94
Philip K. Larsen <kjedtoft@gmail.com>
2023-03-16 14:27:39 +0100

    * Added ObjectId to Map model * Added MongoDB Services

--- src/IndoeNaviAPI/Models/Map.cs
+++ src/IndoeNaviAPI/Models/Map.cs
@@ -1,8 +1,11 @@
-﻿namespace IndoeNaviAPI.Models;
+﻿using MongoDB.Bson;
+
+namespace IndoeNaviAPI.Models;
 
 public class Map
 {
-	public string Area { get; set; }
+	public ObjectId Id { get; set; }
+    public string Area { get; set; }
 	public string ImageData { get; set; } // Endcoded in base64
 	public List<RouteNode> RouteNodes { get; set; }
 	public List<SPE> SPEs { get; set; }
--- src/IndoeNaviAPI/Program.cs
+++ src/IndoeNaviAPI/Program.cs
@@ -1,11 +1,20 @@
+using IndoeNaviAPI.Models;
+using IndoeNaviAPI.Services;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(builder.Configuration.GetConnectionString("mongo_db")));
+builder.Services.AddSingleton<IMongoDBService, MongoDBService>();
 
 var app = builder.Build();
 

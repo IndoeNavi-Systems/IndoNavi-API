c8c3624e11d0c1b8f02453f926b6a2d8891d9c9e
Jonas Hansen <jonas.hansen@visma.com>
2023-03-17 13:05:34 +0100

    Bind services and controllers together

--- src/IndoeNaviAPI/Controllers/MapController.cs
+++ src/IndoeNaviAPI/Controllers/MapController.cs
@@ -1,4 +1,5 @@
 using IndoeNaviAPI.Models;
+using IndoeNaviAPI.Services;
 using IndoeNaviAPI.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,60 +9,32 @@ namespace IndoeNaviAPI.Controllers;
 [Route("[controller]")]
 public class MapController : ControllerBase
 {
-	// Temporary
-	private static readonly Map map = new()
+	private readonly IMapService mapService;
+
+	public MapController(IMapService mapService)
 	{
-		Area = "ZBC Ringsted",
-		RouteNodes = new List<RouteNode>()
-			{
-				new RouteNode
-				{
-					X = 5,
-					Y = 5,
-					IsDestination= true,
-					Name = "D15",
-				}
-},
-		//ImageData = Convert.ToBase64String(System.IO.File.ReadAllBytes("Map.png")),
-		SPEs = new List<SPE>
-			{
-				new SPE
-				{
-					X= 0,
-					Y= 0,
-					Name = "SPE 1"
-				}
-			}
-	};
+		this.mapService = mapService;
+	}
 
 	[HttpGet]
-	public Map GetMap()
+	public async Task<ActionResult<Map>> GetMap(string area)
 	{
+		Map? map = await mapService.GetMap(area);
+		if (map == null)
+		{
+			return NotFound($"No map exist on {area}");
+		}
 		return map;
 	}
 
-	[HttpPut("import")]
-	public ActionResult<Map> ImportMap(string imageData)
+	[HttpPut]
+	public IActionResult UpdateMap(Map map)
 	{
-		if (!Utility.IsBase64String(imageData))
+		if (!Utility.IsBase64String(map.ImageData))
 		{
 			return BadRequest("Image is not encoded in base64");
 		}
-		map.ImageData = imageData;
-		return map;
-	}
-
-	[HttpPut("nodes")]
-	public Map UpdateRouteNodes(List<RouteNode> routeNodes)
-	{
-		map.RouteNodes = routeNodes;
-		return map;
-	}
-
-	[HttpPut("spes")]
-	public Map UpdateSPEs(List<SPE> spes)
-	{
-		map.SPEs = spes;
-		return map;
+		mapService.UpdateMap(map);
+		return Ok();
 	}
 }

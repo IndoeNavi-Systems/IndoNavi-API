7d2a7ab0b85b3cd10c103e56b89f89965fe10e8a
Jonas Hansen <jonas.hansen@visma.com>
2023-06-12 11:45:29 +0200

    Format

--- src/IndoeNaviAPI.Tests/MapServiceTest.cs
+++ src/IndoeNaviAPI.Tests/MapServiceTest.cs
@@ -4,71 +4,71 @@ using Moq;
 
 namespace IndoeNaviAPI.Tests.UnitTests
 {
-    [TestFixture]
-    public class MapServiceTest
-    {
-        [Test]
-        public void UpsertMap_IsAreaNotUnique_TrowDuplicate()
-        {
-            // Arrange
-            Map input = new Map() { ImageData = "asds" }; // asds is not a valid base64 string
+	[TestFixture]
+	public class MapServiceTest
+	{
+		[Test]
+		public void UpsertMap_IsAreaNotUnique_TrowDuplicate()
+		{
+			// Arrange
+			Map input = new Map() { ImageData = "asds" }; // asds is not a valid base64 string
 
-            Mock<IMongoDBService> mockMongoDB = new Mock<IMongoDBService>();
-            mockMongoDB.Setup(m => m.Upsert(input)).ThrowsAsync(new Exception("duplicate"));
-            MapService mapService = new MapService(mockMongoDB.Object);
+			Mock<IMongoDBService> mockMongoDB = new Mock<IMongoDBService>();
+			mockMongoDB.Setup(m => m.Upsert(input)).ThrowsAsync(new Exception("duplicate"));
+			MapService mapService = new MapService(mockMongoDB.Object);
 
-            // Act and Assert
-            var ex = Assert.ThrowsAsync<Exception>(async () => { await mapService.UpsertMap(input); });
-            mockMongoDB.Verify(m => m.Upsert(input), Times.AtLeastOnce);
-            StringAssert.Contains("duplicate", ex.Message);
-        }
+			// Act and Assert
+			var ex = Assert.ThrowsAsync<Exception>(async () => { await mapService.UpsertMap(input); });
+			mockMongoDB.Verify(m => m.Upsert(input), Times.AtLeastOnce);
+			StringAssert.Contains("duplicate", ex.Message);
+		}
 
-        [Test]
-        public void UpsertMap_IsImageStringBase64_TrowFormatException()
-        {
-            // Arrange
-            Map input = new Map() { ImageData = "asd" }; // asd is not a valid base64 string
+		[Test]
+		public void UpsertMap_IsImageStringBase64_TrowFormatException()
+		{
+			// Arrange
+			Map input = new Map() { ImageData = "asd" }; // asd is not a valid base64 string
 
-            Mock<IMongoDBService> mockMongoDB = new Mock<IMongoDBService>();
-            MapService mapService = new MapService(mockMongoDB.Object);
+			Mock<IMongoDBService> mockMongoDB = new Mock<IMongoDBService>();
+			MapService mapService = new MapService(mockMongoDB.Object);
 
-            // Act and Assert
-            var ex = Assert.ThrowsAsync<FormatException>(async () => { await mapService.UpsertMap(input); });
+			// Act and Assert
+			var ex = Assert.ThrowsAsync<FormatException>(async () => { await mapService.UpsertMap(input); });
 
-            StringAssert.Contains("Image is not encoded in base64", ex.Message);
-        }
+			StringAssert.Contains("Image is not encoded in base64", ex.Message);
+		}
 
 
-        [TestCaseSource("GetMockMapList")]
-        public async Task GetMap_IsDestinationExist_ReturnMapObject(Map input)
-        {
-            // Arrange
-            Mock<IMongoDBService> mockMongoDB = new Mock<IMongoDBService>();
-            mockMongoDB.Setup(m => m.GetFirstByKey<Map, string>("Area", input.Area)).ReturnsAsync(() =>
-            {
-                return input;
-            });
-            MapService mapService = new MapService(mockMongoDB.Object);
+		[TestCaseSource("GetMockMapList")]
+		public async Task GetMap_IsDestinationExist_ReturnMapObject(Map input)
+		{
+			// Arrange
+			Mock<IMongoDBService> mockMongoDB = new Mock<IMongoDBService>();
+			mockMongoDB.Setup(m => m.GetFirstByKey<Map, string>("Area", input.Area)).ReturnsAsync(() =>
+			{
+				return input;
+			});
+			MapService mapService = new MapService(mockMongoDB.Object);
 
-            // Act
-            var result = await mapService.GetMap(input.Area);
+			// Act
+			var result = await mapService.GetMap(input.Area);
 
-            // Assert
-            Assert.AreEqual(input.Area, result.Area);
+			// Assert
+			Assert.AreEqual(input.Area, result.Area);
 
-        }
+		}
 
-        private static List<Map> GetMockMapList()
-        {
-            List<Map> maps = new List<Map>
-            {
-                new Map() { Area = "Test1" },
-                new Map() { Area = "Test2" },
-                new Map() { Area = "Test3" },
-                new Map() { Area = "Test4" },
-                new Map() { Area = "Test5" }
-            };
-            return maps;
-        }
-    }
+		private static List<Map> GetMockMapList()
+		{
+			List<Map> maps = new List<Map>
+			{
+				new Map() { Area = "Test1" },
+				new Map() { Area = "Test2" },
+				new Map() { Area = "Test3" },
+				new Map() { Area = "Test4" },
+				new Map() { Area = "Test5" }
+			};
+			return maps;
+		}
+	}
 }
